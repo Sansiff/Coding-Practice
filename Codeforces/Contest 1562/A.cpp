056d874384f45fda28b38107ff6a4d220290eb9b
Sansiff <ljg@henu.edu.cn>
2021-09-14 19:06:07 +0800

    update

---
+++ Codeforces/Contest 1562/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f;
+     
+void solve(){
+    int a, b; cin >> a >> b;
+    if(a >= 2 * b) cout << (a - 1) / 2;
+    else cout << a % b;
+}
+
+int32_t main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while(t --){
+        solve();
+        if(t) cout << endl;
+    }
+    return 0;
+}
